f6f032aad856a704fd40a1859947cb0e194d88a3
iMythms <maythams.149@gmail.com>
2024-10-21 20:46:48 +0300

    Refactor .gitignore and server.js
    - Update .gitignore to exclude /node_modules
    - Add home route in server.js
    - Add greeting route in server.js
    - Implement rolling the dice feature in server.js

--- server.js
+++ server.js
@@ -2,10 +2,24 @@
 const express = require('express')
 const app = express()
 
-// Greet the user
-app.use('/greetings/:name', (req, res) => {
-	//
+// Home
+app.use('./', (req, res) => {
+	res.send('<h1>Home Page</h1>')
 })
+
+/* 
+Exercise 1:
+Greet the user
+*/
+app.use('/greetings/:name', (req, res) => {
+	const name = req.params.name
+	res.send(`Hello there, ${name}!`)
+})
+
+/* 
+Exercise 2:
+Rolling the Dice
+*/
 
 app.listen(3000, () => {
 	console.log('Listening to port 3000')
